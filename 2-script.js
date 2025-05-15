04f21c6aa3fd3f6dc967230e219a944736b59b3e
Narupalle_swapna <nswapna.fullstack@gmail.com>
2025-05-15 21:19:19 +0530

    JS_2nd_class

---
+++ 2-script.js
@@ -0,0 +1,46 @@
+//Primitive data types
+//1.number data type----------------------------
+/*
+let age = 25;// number
+
+let percentage = 30.35;// number
+
+let length = 10.5;// number
+
+console.log(age);
+console.log(percentage);
+console.log(length);
+*///----------------------------
+
+//2.string data type----------------------------------
+/*let technologyname = "Javascript";// string
+console.log(technologyname);
+
+let technologyPosition = 1;
+let aboutJSTechnology  = `Javascript is a ${technologyPosition} programming language`;
+
+console.log(aboutJSTechnology);
+*/
+/*let technologyname = "Javascript";
+let technologyPosition = 1;
+let aboutJS = `${technologyname} is a programming language. It is a no ${technologyPosition} in It market.`;
+console.log(aboutJS);
+*///-----------------------------------------------------
+//3.boolean data type---------------------------------------
+/*let isActive = true;//boolean
+console.log(isActive);
+
+let isStudent =false;
+console.log(isStudent);
+*///------------------------------------------------
+//4.undefined data type---------------------------------
+/*let name;
+console.log(name);// undefined
+*///-------------------------------------------
+//5.null data type-------------------------------------
+/*let name = null;
+console.log(name);// null
+*///--------------------------------------
+//6.symbol data type
+let id = Symbol(123);// symbol
+console.log(id);
