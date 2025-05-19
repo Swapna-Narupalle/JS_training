cf155ff2e1d70a696403affb056e346451112970
Narupalle_swapna <nswapna.fullstack@gmail.com>
2025-05-19 22:00:49 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,61 @@
+// functions
+/*  Block1  ---------------------------------------------------------------
+function bookDetails() {
+    bookNames = ["Mahabaratha", "Ramayana", "Bagavath_Gita"];
+    bookAuthors = ["Vyasa", "Valmiki", "Krishna"];
+    console.log(bookNames);
+    console.log(bookAuthors[2]);
+    console.log(bookAuthors);
+    bookNames.push("Kuran");
+
+}
+bookDetails();
+*///-----------------------------------------------------------------
+
+
+//Block2
+/*--------------------------------------------------------------------
+function studentDetails() {
+    let studentName = "Swapna";
+    console.log(studentName);
+    
+    let studentId = 484;
+    console.log(studentId);
+    }
+studentDetails();
+*///-------------------------------------------------------------------
+
+
+//Block3
+///*-------------------------------------------------------------------
+function redcolor() {
+    console.log("%c      ","background-color:red");
+}
+redcolor();
+
+
+function firstColor() {
+    console.log("%c      ","background-color:black");
+    redcolor();
+}
+firstColor();
+
+
+function secondColor() {
+    console.log("%c      ","background-color:green");
+    redcolor();
+}
+secondColor();
+
+
+function thirdColor() {
+    console.log("%c      ","background-color:pink");
+    redcolor();
+}
+thirdColor();
+
+    
+
+
+
+
