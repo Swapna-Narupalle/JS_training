785cebfbf965c1464a7978fceeba455862a58223
Narupalle_swapna <nswapna.fullstack@gmail.com>
2025-05-20 22:14:02 +0530

    return type

---
+++ functions/returnfun.js
@@ -0,0 +1,40 @@
+//Return type function
+/*-----------------------------------------
+function movieDetails(movieName, actorName) {
+    let movieDetails = "";
+    movieDetails ="movieName:" + movieName + "   " + "actorName:" + actorName;
+    return movieDetails;
+}
+console.log(movieDetails("Devara", "NTR"));
+let getmovieDetails = movieDetails("Devara", "NTR");
+console.log(getmovieDetails);
+*///------------------------------------------
+
+
+/*-----------------------------------------------
+function sum() {
+    X = 200;
+    Y=300;
+    Z=X+Y;
+}
+sum();
+console.log("Sum of X and Y is: " + Z);
+
+
+
+let multiply = function mul(X, Y) {
+    return X*Y;
+}
+console.log("Multiplication of X and Y is: " + multiply(X, Y));
+
+
+
+let divide = function div(X,Y) {
+    return X/Y;
+}
+console.log("Division of X and Y is: " + divide(X, Y));
+*///-------------------------------------------------
+
+
+
+
