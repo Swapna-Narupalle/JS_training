89cd9b46b233d15280f8c8efb88e2d864ebc5ab2
Narupalle_swapna <nswapna.fullstack@gmail.com>
2025-05-23 21:36:33 +0530

    debugging

--- debugging.js
+++ debugging.js
@@ -24,3 +24,21 @@ function statepolyticsDetails(Andhrapradesh, Telangana, Gujarath) {
       document.write("Gujarath:" + Gujarath);
 }
 statepolyticsDetails("TDP", "Congress", "BJP");
+
+function showstudentName(studentName) {
+      studentName();
+}
+
+//callback
+showstudentName(function() {
+      console.log("studentName:" + "Swapna");
+});
+
+
+let vegitableDetails = function(vegitables) {
+      vegitables("potato", "carrot");
+}
+//callback
+vegitableDetails(function(X,Y) {
+      console.log(X,Y);
+});
