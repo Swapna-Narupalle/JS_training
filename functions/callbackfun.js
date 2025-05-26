dbebe453f9b81b0ae921dde60a1fbdcff0d55b7b
Narupalle_swapna <nswapna.fullstack@gmail.com>
2025-05-26 15:45:41 +0530

    call back

--- functions/callbackfun.js
+++ functions/callbackfun.js
@@ -27,3 +27,53 @@ function PrintMessage(fnParam){
      console.log(p1);
  })
 
+
+ function foodorderDetails(itemNames) {
+    itemNames("Gobi" + "," + "Biryani" + "," + "Panipuri");
+    
+
+ }
+ foodorderDetails(function(items) {
+   console.log(items);
+ });
+
+
+
+function studentName(name) {
+    name();
+    //console.log(callback);
+}
+studentName(function() {
+    let name = "Swapna";
+    console.log(name);
+});
+
+
+
+function showmovieDetails(movieDetails) {
+    movieDetails();
+}
+showmovieDetails(function() {
+    let movieDetails = {
+        name:"RRR",
+        actor:"NTR",
+        director:"Rajamouli"
+    };
+    console.log(movieDetails);
+    
+});
+
+
+function showmovieDetails(movieDetails) {
+   result = movieDetails();
+   console.log(result);
+}
+showmovieDetails(function() {
+    let movieDetails = {
+        name:"RRR",
+        actor:"NTR",
+        director:"Rajamouli"
+    };
+    return movieDetails;
+    
+});
