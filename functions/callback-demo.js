db43e8cfd86c75c434dda76d9b6757e87b0a026e
Narupalle_swapna <nswapna.fullstack@gmail.com>
2025-06-01 18:27:50 +0530

    callback

---
+++ functions/callback-demo.js
@@ -0,0 +1,44 @@
+//function
+function movieName() {
+    console.log("RRR");
+}
+//movieName();//RRR
+
+//callback Functions
+
+function add(a,b) {
+    return a+b;
+}
+
+function sub(a,b) {
+    return a-b;
+}
+
+function mul(a,b) {
+    return a*b;
+}
+
+function div(a,b) {
+    return a/b;
+}
+
+function calc(a,b,callback) {
+    return callback(a,b);
+}
+//console.log(calc("20",10));
+// console.log(calc(20,10,sub));
+// console.log(calc(20,10,mul));
+// console.log(calc(20,10,div));
+
+
+function showmovieDetails(callback) {
+    let details = callback();
+    console.log(details);
+}
+showmovieDetails(() => {
+    let movieDetails = {
+        movieName: "RRR",
+        movieActor:"NTR"
+    };
+    return `movieName:${movieDetails.movieName} , movieActor:${movieDetails.movieActor}`;
+});
